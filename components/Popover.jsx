b5571440e262a8784ca5bd33314f3bb36c8406e8
Atayev <atayev25@gmail.com>
2022-11-30 19:38:04 +0400

    added redux,lang,curr pagesettings to redux store, settings button functionality

--- components/Popover.jsx
+++ components/Popover.jsx
@@ -3,48 +3,64 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import styles from '../styles/Nav.module.css'
 import Image from "next/image";
-const Pop = ({id,open,lang,closeLangMenu}) => {
-   
+import { useDispatch,useSelector } from "react-redux";
+import { setLanguage } from "../redux/slices/pageSlice";
+const Pop = ({visible,anchorEl,closeMenu}) => {
+    const dispatch = useDispatch()
+    const lang = useSelector(state => state.pageSettings.lang)
+    const handleChange = (e) => {
+        dispatch(setLanguage({
+            lang:e.target.value
+        }))
+        closeMenu()
+    }
     return ( 
         <Popover
-            id={id}
-            open={open}
-            anchorEl={lang}
-            onClose={closeLangMenu}
+            open={visible}
+            anchorEl={anchorEl}
+            onClose={closeMenu}
             anchorOrigin={{
               vertical: "bottom",
-              horizontal: -20,
+              horizontal: -35,
             }}
           >
-            <div className={styles.popup}>
-              <MenuItem value={10}>
+            <Select className={styles.popup}
+                onChange={handleChange}
+                value={lang}
+            >
+              <MenuItem value='eng'>
                 <span>Eng</span>
                 <Image
                   src="/eng.png"
                   width={20}
                   height={20}
-                  className={styles.lang}
+                        className={styles.lang}
+                        alt='eng'
                 />
               </MenuItem>
-              <MenuItem value={20}>
+              <MenuItem value='aze'>
                 <span>Aze</span>
                 <Image
                   src="/aze.png"
                   width={20}
                   height={20}
-                  className={styles.lang}
+                        className={styles.lang}
+                        alt='aze'
+                        
                 />
-              </MenuItem>
-              <MenuItem value={30}>
+              </MenuItem>  
+              <MenuItem value='rus'>
                 <span>Rus</span>
                 <Image
                   src="/rus.png"
                   width={20}
                   height={20}
-                  className={styles.lang}
+                        className={styles.lang}
+                        alt='rus'
+                        
                 />
               </MenuItem>
-            </div>
+            </Select>
           </Popover>
      );
 }
