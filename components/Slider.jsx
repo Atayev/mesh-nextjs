ca1ceef7778a2c9cd51c215671d741e642cb107c
Atayev <atayev25@gmail.com>
2022-12-02 19:35:28 +0400

    slider

---
+++ components/Slider.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Carousel from 'react-material-ui-carousel'
+import Image from 'next/image'
+import { Paper, Button, Box } from '@mui/material'
+
+function Slider({items}) {
+  return (
+      <Carousel
+          autoPlay
+          swipe
+          cycleNavigation
+          animation='slide'
+          activeIndicatorIconButtonProps={{
+              style: {
+                  color: '#d8127d'
+              }
+          }
+        }
+        indicatorContainerProps = {{
+            style: {
+                zIndex:1,
+                marginTop: '-20px',
+                position: 'relative',
+                opacity:0.9
+          }
+        }}
+      >
+          {items.map(item => (
+              <Paper>
+                  <Image  src={item.imageUrl} key={item.title} width={1394} height={450} quality={100} />
+              </Paper>
+          ))}
+    </Carousel>
+  )
+}
+
+export default Slider
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+import Test from '../components/test'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:3000/data.json')
+  const data = await res.json()
+
+  return {
+    props: {
+      slider:data.slider
+    }
+  }
+}
+
+
+export default function Home({slider}) {
   return (
     <div className={styles.main}>
       <Navbar />
-      
+      <Slider items={slider}/>
     </div>
   )
 }
