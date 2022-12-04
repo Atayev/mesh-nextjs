a44d4fbf4340403f1eab372b4513601496ad285c
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2022-12-04 16:24:45 +0400

    fix

---
+++ components/SliderItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Paper, Button } from "@mui/material";
+
+function SliderItem() {
+  return (
+    <Paper>
+      <h2>''</h2>
+      <p>'</p>
+
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  );
+}
+
+export default SliderItem;
