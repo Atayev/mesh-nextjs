5e25f448a0b440f362a96bc24f83713da726891d
Atayev <atayev25@gmail.com>
2022-12-05 14:43:52 +0400

    productitem fix

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -1,11 +1,15 @@
 import { Box } from "@mui/material";
 import Image from "next/image";
 import styles from "../styles/Product.module.css";
+import FavoriteBorderOutlinedIcon  from '@mui/icons-material/FavoriteBorderOutlined';
 const ProductItem = ({ imageUrl, title, price }) => {
   return (
     <Box className={styles.productItem}>
           <img src={imageUrl} alt='title' />
+          <Box className={styles.productItemNav} display='flex' justifyContent='center'>
           <button className={styles.productItemButton}>Add to Cart</button>
+          <FavoriteBorderOutlinedIcon />
+          </Box>
         <Box display="flex" justifyContent="space-between">
         <p className={styles.productTitle}>{title}</p>
         <p className={styles.productPrice}>{price}</p>
