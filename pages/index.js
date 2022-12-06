36f604e712043d8f4dd58b2fb2274f2cb181ac5e
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2022-12-06 22:36:26 +0400

    pull

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,8 @@ import styles from "../styles/Home.module.css";
 import { useState } from "react";
 
 import Products from "./products";
-<<<<<<< HEAD
-import Footer from '../parts-pages/Footer'
-
-=======
 import Layout from "../components/Layout";
 import Loader from "../components/Loader";
->>>>>>> a1bc4bc933dea06add5e4877b3a60df39e2ff7c7
 export const getStaticProps = async () => {
   const res = await fetch("http://localhost:3000/data.json");
   const data = await res.json();
@@ -27,9 +22,7 @@ export default function Home({ slider, products }) {
   const [isLoading, setIsLoading] = useState(false);
   console.log(products);
   if (isLoading) {
-    return (
-     <Loader />
-    );
+    return <Loader />;
   }
 
   return (
