51b366dbae022fa95e5d7f550a12b4726d7fa48b
Atayev <atayev25@gmail.com>
2022-12-07 15:06:01 +0400

    connected mongodb

--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,46 @@ import Head from "next/head";
 import Slider from "../components/Slider";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
+import {initMongoose} from '../utils/dbConnect.js'
 
 import Products from "./products";
 import Layout from "../components/Layout";
 import Loader from "../components/Loader";
-export const getStaticProps = async () => {
-  const res = await fetch("http://localhost:3000/data.json");
-  const data = await res.json();
+import { getAllProducts } from "./api/products";
 
+export const getServerSideProps = async () => {
+  await initMongoose();
+  const data = await getAllProducts()
   return {
     props: {
-      slider: data.slider,
-      products: data.products,
+      products: JSON.parse(JSON.stringify(data)),
     },
   };
 };
+
+// export const getStaticProps = async () => {
+//   const res = await fetch("http://localhost:3000/data.json");
+//   const data = await res.json();
+
+//   return {
+//     props: {
+//       slider: data.slider,
+//       products: data.products,
+//     },
+//   };
+// };
 
 export default function Home({ slider, products }) {
   const [isLoading, setIsLoading] = useState(false);
   console.log(products);
   if (isLoading) {
-    return (
-     <Loader />
-    );
+    return <Loader />;
   }
 
   return (
     <div className={styles.main}>
       <Layout>
-        <Slider items={slider} />
+        {/* <Slider items={slider} /> */}
         <Products products={products} />
       </Layout>
     </div>
