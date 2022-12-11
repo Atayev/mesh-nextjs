3c350fa2b120405d41d572a888185b5b9e40afd4
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2022-12-11 18:20:32 +0400

    pull

--- redux/store.js
+++ redux/store.js
@@ -1,38 +1,36 @@
-import { configureStore,   } from '@reduxjs/toolkit'
-import pageSlice from './slices/pageSlice';
-import storage from 'redux-persist/lib/storage';
+import { configureStore } from "@reduxjs/toolkit";
+import pageSlice from "./slices/pageSlice";
+import storage from "redux-persist/lib/storage";
+import cartSlice from "./slices/cartSlice";
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist'
-import cartSlice from './slices/cartSlice';
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const persistConfig = {
-    key: 'root',
-    storage,
-}
+  key: "root",
+  storage,
+};
 
-const persistedReducer = persistReducer(persistConfig, pageSlice)
-const persistedReducerCart = persistReducer(persistConfig, cartSlice)
-
+const persistedReducer = persistReducer(persistConfig, pageSlice);
+const persistedReducerCart = persistReducer(persistConfig, cartSlice);
 
 export const store = configureStore({
-    reducer: {
-        pageSettings: persistedReducer,
-        cart:persistedReducerCart
-    },
-    middleware: (getDefaultMiddleware) => 
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions:[FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-            }
-        })
-    
-})
-export const persistor = persistStore(store)
+  reducer: {
+    pageSettings: persistedReducer,
+    cart: persistedReducerCart,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
